1d0298c23250278a45c98072f37ac7debbb15c6e
mirackeskinn <mirackeskin003@gmail.com>
2025-07-28 17:50:08 +0300

    admin panel

--- rde-frontend/src/App.tsx
+++ rde-frontend/src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>src/App.tsx</code> dosyasını düzenleyin ve kaydedin, sayfa otomatik olarak yenilenecektir.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React'i Öğren
-        </a>
-      </header>
-    </div>
+    <Admin dataProvider={simpleRestProvider('http://localhost:5103/api')}>
+      <Resource name="users" list={ListGuesser} />
+    </Admin>
   );
 }
 
